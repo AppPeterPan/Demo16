7c7244cba38f4a3d6fb5bb26fc55165440141765
peter pan <apppeterpan@gmail.com>
2022-07-16 21:40:13 +0800

    Initial Commit

---
+++ Demo/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Demo
+//
+//  Created by Peter Pan on 2022/7/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Demo/DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoApp.swift
+//  Demo
+//
+//  Created by Peter Pan on 2022/7/16.
+//
+
+import SwiftUI
+
+@main
+struct DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
